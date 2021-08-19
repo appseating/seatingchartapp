4930d42b2e73a5818a328ddaa1e76ff7a3c9a79c
Tae Kyu Kim <taekyukim02@gmail.com>
2021-08-19 15:11:48 -0700

    Create tables in Hub page

--- web-app/src/components/Hub/index.tsx
+++ web-app/src/components/Hub/index.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
 import {k_hub_link} from "../Home";
 import {Container} from "react-bootstrap";
+import ISeatingChart from "../Interfaces/ISeatingChart";
+import CustomListTable from "./CustomListTable";
 
 interface HomeProps {
     screen: string;
@@ -13,6 +15,89 @@ const Hub = (props: HomeProps) => {
         props.setScreen(k_hub_link);
     }, [props]);
 
+    // hard coded data for testing purposes
+    const testSeatingChart1: ISeatingChart = {
+        id: "someSeatingChartId1",
+        name: "someSeatingChartName1",
+        numRows: 2,
+        numColumns: 2,
+        tableGroups: [
+            {
+                id: "someTableId1",
+                name: "someTableName1",
+                row: 1,
+                column: 1,
+                students: [
+                    {
+                        id: "someStudentId1",
+                        firstName: "Bob",
+                        lastName: "Jones"
+                    },
+                    {
+                        id: "someStudentId2",
+                        firstName: "Bobby",
+                        lastName: "Jones"
+                    }
+                ]
+            },
+            {
+                id: "someTableId2",
+                name: "someTableName2",
+                row: 1,
+                column: 2,
+                students: [
+                    {
+                        id: "someStudentId3",
+                        firstName: "Jones",
+                        lastName: "Bob"
+                    }
+                ]
+            }
+        ]
+    };
+    const testSeatingChart2: ISeatingChart = {
+        id: "someSeatingChartId1",
+        name: "someSeatingChartName1",
+        numRows: 2,
+        numColumns: 2,
+        tableGroups: [
+            {
+                id: "someTableId1",
+                name: "someTableName1",
+                row: 1,
+                column: 1,
+                students: [
+                    {
+                        id: "someStudentId1",
+                        firstName: "Bob",
+                        lastName: "Jones"
+                    },
+                    {
+                        id: "someStudentId2",
+                        firstName: "Bobby",
+                        lastName: "Jones"
+                    }
+                ]
+            },
+            {
+                id: "someTableId2",
+                name: "someTableName2",
+                row: 1,
+                column: 2,
+                students: [
+                    {
+                        id: "someStudentId3",
+                        firstName: "Jones",
+                        lastName: "Bob"
+                    }
+                ]
+            }
+        ]
+    };
+    const seatingCharts1 = [testSeatingChart1, testSeatingChart2];
+    const seatingCharts2 = [testSeatingChart1, testSeatingChart2];
+
+
     return (
         <div className={"page-container"}>
             <Container>
@@ -20,6 +105,16 @@ const Hub = (props: HomeProps) => {
 				<div>
 				We will fill this page with lists of in-progress and completed class layouts and rosters. It will also link to other "Create" pages.
 				</div>
+                <br/>
+                <CustomListTable
+                    title="Seating Layouts"
+                    seatingCharts={seatingCharts1}
+                />
+                <br/>
+                <CustomListTable
+                    title="Class Rosters"
+                    seatingCharts={seatingCharts2}
+                />
             </Container>
         </div>
     );
